94a501595662f215e4f98d56414a3717301a78a4
homer0 <leo.apiwan@olapic.com>
2018-04-01 04:01:16 -0300

    webpack/react: webapp

---
+++ webpack/react/src/webapp/components/main/main.component.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import Hello from '../hello/hello.component';
+import './main.scss';
+
+const main = () => (
+  <div className="main">
+    <Hello />
+  </div>
+);
+
+export default main;

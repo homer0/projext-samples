42bde3034fa2142c1d079020a989f7648a3515ca
homer0 <leo.apiwan@olapic.com>
2018-04-14 05:33:39 -0300

    webpack/basic: add font-awesome and normalize.css to the webappcss target

--- webpack/basic/src/webappcss/index.js
+++ webpack/basic/src/webappcss/index.js
@@ -1,6 +1,8 @@
 const hello = require('./hello.html');
 const styles = require('./assets/styles.scss');
 require('./assets/favicon.ico');
+require('normalize.css');
+require('font-awesome/css/font-awesome.css');
 
 // Get the DOM element where the app will be rendered.
 const app = document.querySelector('#app');
